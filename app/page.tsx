36975211ed2f016bcfd1d5d0d9bea4e3ce98600d
Neyito <207490469+Neyito96@users.noreply.github.com>
2026-09-13 16:33:45 +0900

    3択UIブリッジを朝リスト画面へ接続

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 import Community from "./community";
+import PlaylistEntryModeBridge from "./PlaylistEntryModeBridge";
 import { playlists } from "./data";
+
 export const dynamic = "force-static";
-export default function Home(){ return <Community playlists={playlists}/>; }
+
+export default function Home() {
+  return (
+    <>
+      <Community playlists={playlists} />
+      <PlaylistEntryModeBridge />
+    </>
+  );
+}
